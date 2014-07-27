14f2292602aac3d49775a14d2d5a16b6dccb607b
tbulligan <tomaso.bulligan@gmail.com>
2014-07-27 18:43:15 +0200

    Changed file output from .csv to .txt

--- run_analysis.R
+++ run_analysis.R
@@ -1,33 +1,39 @@
+## Initial setup
+
 # Define file name variables
 data.file <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
 local.data.file <- './original-dataset.zip'
 local.data.dir <- './UCI HAR Dataset'
-tidy.data.file <- './tidy-UCI-HAR-dataset.csv'
-tidy.avgs.data.file <- './tidy-UCI-HAR-avgs-dataset.csv'
+tidy.data.file <- './tidy-UCI-HAR-dataset.txt'
+tidy.avgs.data.file <- './tidy-UCI-HAR-avgs-dataset.txt'
 
-## Data collection
+## Retrieve data
+
+print("Retrieving data...")
 
 # Check if starting data exists, else download
 if (!file.exists(local.data.file)) {
   download.file(data.file, local.data.file, method = "curl")
-  print(paste(local.data.file, "downloaded"))
 }
 
 # Check data retrieval successful, else stop
 if (!file.exists(local.data.file)) {
   stop(paste("Could not retrieve", local.data.file))
 }
-  
+
 # Check if starting data is uncompressed, else uncompress
 if (!file.exists(local.data.dir)) {
   unzip(local.data.file)
-  print(paste(local.data.file, "uncompressed"))
 }
 
 # Check if uncompression successful, else stop
 if (!file.exists(local.data.dir)) {
   stop(paste("Could not extract data from", local.data.file))
 }
+
+## Read data to global environment
+
+print("Processing data...")
 
 # Read activity labels
 activity.labels <- read.table(paste(local.data.dir, "activity_labels.txt", sep = "/"), col.names = c("id", "activity"))
@@ -68,7 +74,8 @@ temp.labels$activity <- activity.labels[temp.labels$activity,]$activity
 tidy.data.set <- cbind(subjects, temp.labels, temp.set)
 
 # Dump data set containing merged training and test data
-write.csv(tidy.data.set, tidy.data.file)
+print(paste("Dumping ", tidy.data.file, "...", sep = ""))
+write.table(tidy.data.set, tidy.data.file, sep = ",", row.names = FALSE)
 
 # Compute the averages grouped by subject and activity
 tidy.avgs.data.set <- aggregate(tidy.data.set[, 3:dim(tidy.data.set)[2]],
@@ -78,4 +85,8 @@ tidy.avgs.data.set <- aggregate(tidy.data.set[, 3:dim(tidy.data.set)[2]],
 names(tidy.avgs.data.set)[1:2] <- c('subject', 'activity')
 
 # Dump data set containing averages grouped by subject and activity
-write.csv(tidy.avgs.data.set, tidy.avgs.data.file)
+print(paste("Dumping ", tidy.avgs.data.file, "...", sep = ""))
+write.table(tidy.avgs.data.set, tidy.avgs.data.file, sep = ",", row.names = FALSE)
+
+# End
+print("Done.")
